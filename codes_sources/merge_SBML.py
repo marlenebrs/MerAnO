f8f686965b33d9b6c0717e418f3bd659671e4d98
cmathez <61285819+cmathez@users.noreply.github.com>
2020-05-10 17:48:51 +0200

    Update merge_SBML.py

--- codes_sources/merge_SBML.py
+++ codes_sources/merge_SBML.py
@@ -1,2 +1,63 @@
 import libsbml
 
+### Step 1 : Get document and model
+
+def read_sbml(file):
+  """
+  return model of SBML file 
+  """
+    document = libsbml.readSBML(file)
+    if document.getNumErrors() > 0:
+        print("Encountered the following SBML errors:" + "\n")
+        document.printErrors()
+    else:       
+        return document
+      
+def get_model(document):
+  model = document.getModel()
+  return model
+
+## Step 2 : Define Id for one organism
+
+# def get_Id_organism(model):
+#    return Id
+
+## Step 3 :  Get each object
+
+# def get_ListOfFunctionDefinitions(model):
+#   return ListOfFunctionDefinitions
+
+# def get_ListOfUnitDefinitions(model):
+#   return ListOfUnitDefinitions
+
+# def get_ListOfCompartments(model):
+#   return ListOfCompartments
+
+# def get_listOfSpecies(model):
+#   return listOfSpecies
+
+# def get_ListOfParameters(model):
+#   return ListOfParameters
+
+# def get_ListOfReactions(model):
+#   return ListOfReactions
+
+## Step 4 : Modify Id in a model
+
+# def modify_Id(model):
+#   return model
+
+## Step 5 : Create new SBML to merge all documents
+
+# Object
+# ListOfFunctionDefinitions, 
+# ListOfUnitDefinitions, 
+# ListOfCompartments, 
+# ListOfSpecies, 
+# ListOfParameters, 
+# ListOfInitialAssignments, 
+# ListOfRules, 
+# ListOfConstraints, 
+# ListOfReactions,
+# ListOfEvents.
+#
