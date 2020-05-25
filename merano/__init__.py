0f34e391381f35a61ea89a64818cf849f6f5669c
cmathez <61285819+cmathez@users.noreply.github.com>
2020-05-25 22:30:33 +0200

    Update __init__.py

--- merano/__init__.py
+++ merano/__init__.py
@@ -1,3 +1,3 @@
-from merano.annotation import make_dict
-from merano.annotation import get_name_org
+from merano.request_database import make_dict
+from merano.data_processing import get_name_org
 from merano.merge_SBML import all
