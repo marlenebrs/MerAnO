3e442c855350fb53be2dd6fa5e25a19bf39430ce
cmathez <61285819+cmathez@users.noreply.github.com>
2020-05-26 18:48:10 +0200

    Update __init__.py

--- merano/__init__.py
+++ merano/__init__.py
@@ -1,6 +1,7 @@
 from merano.request_database import make_dict
 from merano.data_processing import get_name_org
 from merano.merge_SBML import main_sbml, read_sbml
+from merano.main_annotation import main_analyses
 
 __version__ ="0.0.1"
 """Define the version of the package.
