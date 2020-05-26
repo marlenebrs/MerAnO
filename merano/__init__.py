d985fad6652c75395524678c4852fc4b54850345
cmathez <61285819+cmathez@users.noreply.github.com>
2020-05-26 18:33:09 +0200

    Update __init__.py

--- merano/__init__.py
+++ merano/__init__.py
@@ -1,3 +1,3 @@
 from merano.request_database import make_dict
 from merano.data_processing import get_name_org
-from merano.merge_SBML import main_SBML, read_sbml
+from merano.merge_SBML import main_sbml, read_sbml
