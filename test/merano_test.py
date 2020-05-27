7bcdeaad4dadf59520c63ca3e9405d69dae85fa4
Marie-Ancelle <marie.ancelle@wanadoo.fr>
2020-05-27 12:39:30 +0200

    update merano_test.py

--- test/merano_test.py
+++ test/merano_test.py
@@ -8,7 +8,7 @@ import libsbml
 
 from merano import main_sbml, read_sbml
 #from merano import main_analysis
-from merano import read_text
+from merano import read_text,request_kegg
 from merano import get_name_org
 
 def test_filetype(filename, filetype):
@@ -21,6 +21,11 @@ def test_main_SBML():
     results = main_sbml(files)
     test_filetype(results, filetype)
 
+def test_read_sbml():
+    #filetype = 'libsbml.SBMLDocument'
+    filename = 'Samples/SBML_files/bacteroides_fragilis_YCH46.xml'
+    results = read_sbml(filename)
+    assert isinstance(results, libsbml.SBMLDocument)
       
 def test_annotation():
     dict = {'Bacteroides_fragilis_YCH46': {'Carbohydrate metabolism': 8, 'Metabolism of cofactors and vitamins': 4, 'Energy metabolism': 2, 'Amino acid metabolism': 5}, 'Enterococcus_faecalis_V583': {'Nucleotide metabolism': 2, 'Gene set': 1, 'Amino acid metabolism': 5, 'Metabolism of cofactors and vitamins': 2, 'Biosynthesis of terpenoids and polyketides': 2}, 'Lactobacillus_plantarum_WCFS1': {'Amino acid metabolism': 2, 'Carbohydrate metabolism': 6, 'Metabolism of cofactors and vitamins': 5, 'Energy metabolism': 2, 'Lipid metabolism': 1, 'Nucleotide metabolism': 1, 'Gene set': 1}}
@@ -31,13 +36,24 @@ def test_annotation():
     print (data)
     assert data == dict
 
-def test_read_sbml():
-    #filetype = 'libsbml.SBMLDocument'
-    filename = 'Samples/SBML_files/bacteroides_fragilis_YCH46.xml'
-    results = read_sbml(filename)
-    assert isinstance(results, libsbml.SBMLDocument)
+def test_get_name_org():
+    filename='MerAnO/test/Samples/Annotation_files/Bacteroides_fragilis_YCH46.fa.emapper.annotations'
+    name=filename.split(".")[0]
+    name=name.split("/")[-1]
+    results=get_name_org(filename)
+    assert results == name
+
+def test_read_text():
+    module='M00088'
+    text=request_kegg(module)
+    list=['Pathway modules', 'Lipid metabolism', 'Lipid metabolism', 'Ketone body biosynthesis, acetyl-CoA => acetoacetate/3-hydroxybutyrate/acetone', 'M00088']
+    results=read_text(module,text)
+    assert results == list
+    
 
 test_read_sbml()
 #test_main_SBML()
-test_annotation()
+#test_annotation()
+test_get_name_org()
+test_read_text()
 print('Done testing.')
