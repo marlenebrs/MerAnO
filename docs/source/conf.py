a9edfbac868274a7ff4b19e4007ee23f607faf53
cmathez <61285819+cmathez@users.noreply.github.com>
2020-05-27 23:09:29 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../merano'))
+sys.path.insert(0, os.path.abspath('../../merano'))
 
 
 # -- Project information -----------------------------------------------------
