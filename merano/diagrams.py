3127f406301b460d470405cfc4f942de2834b6fc
Marie-Ancelle <marie.ancelle@wanadoo.fr>
2020-05-27 19:58:31 +0200

    update diagrams.py

--- merano/diagrams.py
+++ merano/diagrams.py
@@ -30,21 +30,21 @@ def make_multiple_barplot(modules,label,value):
     width=0.8
     #fig, ax = plt.subplots()
     n=len(value)
-    fig, ax = plt.subplots(figsize=(10,8))
+    fig, ax = plt.subplots()
     for i in range(n):
         bar_plot=plt.bar(x - width/2. + i/float(n)*width, value[i], 
-                width=width/float(n), align="edge", label=label[i])
+                width=width/float(n), align="edge")
         for idx,rect in enumerate(bar_plot):
             height = rect.get_height()
             ax.text(rect.get_x() + rect.get_width()/2., 1*height,value[i][idx],
-                    ha='center', va='bottom', rotation=0,fontsize=7)
+                    ha='center', va='bottom', rotation=0,fontsize=6)
     
-    plt.xticks(x,modules,rotation=45,horizontalalignment='right',fontweight='light')
     plt.title('Division of metabolisms within the bacteria')
+    plt.xticks(x,modules,rotation=45,horizontalalignment='right',fontweight='light')
     plt.xlabel('Modules present in the bacteria\'s metabolism')
     plt.ylabel('Occurences of the modules within the bacteria metabolism')
     plt.tight_layout()
-    plt.legend(label)
+    plt.legend(label, fontsize=8)
     name='multiple_barplot'
     plt.savefig('./Results/'+name, format='png')
     plt.close()
@@ -70,18 +70,18 @@ def make_barplot(modules,label,value):
     x=np.arange(len(modules))
     width=0.8
 
-    bar_plot=plt.bar(x , value,width, align="edge", label=label)
-    plt.xticks(x,modules,rotation=45,horizontalalignment='right',fontweight='light')
     fig, ax = plt.subplots(figsize=(10,8))
+    bar_plot=plt.bar(x , value,width, align="edge")
+    
     for idx,rect in enumerate(bar_plot):
         height = rect.get_height()
         ax.text(rect.get_x() + rect.get_width()/2., 1*height,value[idx],
-                ha='center', va='bottom', rotation=0)
+                ha='center', va='bottom', rotation=0,fontsize=8)
+        plt.xticks(x,modules,rotation=45,horizontalalignment='right',fontweight='light',fontsize=7)
     plt.title('Division of metabolisms within ' + label)
-    plt.tight_layout()
-    plt.legend()
     plt.xlabel('Modules present in the bacteria\'s metabolism')
     plt.ylabel('Occurences of the modules within the bacteria metabolism')
+    plt.tight_layout()
     name=('barplot_'+label)
     plt.savefig('./Results/'+name, format='png')
     plt.close()
@@ -111,6 +111,7 @@ def make_pie(module,label,value):
     bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
     wedges, texts = ax.pie(values, wedgeprops=dict(width=1), startangle=+40)
     kw = dict(arrowprops=dict(arrowstyle="-"),bbox=bbox_props, zorder=0, va="center")
+
     for i, p in enumerate(wedges):
         percent = 100.*values/sum(values)
         ang = (p.theta2 - p.theta1)/2. + p.theta1
@@ -119,10 +120,10 @@ def make_pie(module,label,value):
         horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
         connectionstyle = "angle,angleA=0,angleB={}".format(ang)
         kw["arrowprops"].update({"connectionstyle": connectionstyle})
-        ax.annotate(['{0} - {1:1.2f} %'.format(v,n) for v,n in zip(modules, percent)][i], xy=(x, y), 
-                    xytext=((i/10)+0.2*np.sign(x), 1.3*y),horizontalalignment=horizontalalignment, **kw)
+        ax.annotate(['{0} \n {1:1.2f} %'.format(v,n) for v,n in zip(modules, percent)][i], xy=(x, y), 
+                    xytext=(1.5*np.sign(x), 1.6*y),horizontalalignment=horizontalalignment,fontsize=7 **kw)
 
-    ax.set_title('Division of metabolisms within '+label)
+    ax.set_title('Division of metabolisms within '+label,y=1.2)
     #plt.legend(modules, bbox_to_anchor=(2,0), loc="lower left", bbox_transform=plt.gcf().transFigure)
     name=('pie_'+label)
     plt.savefig('./Results/'+ name, format='png')
